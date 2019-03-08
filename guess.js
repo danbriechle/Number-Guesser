ba4705080cbf07ddbb18a2d4dd71c10b5e5f16d7
Daniel Briechle <danbriechle@hotmail.com>
2019-03-07 22:22:26 -0700

    adds various functions

--- guess.js
+++ guess.js
@@ -1,22 +1,43 @@
 
   //randomly makes the correct number which the user will try and guess
-  var correctNumber = (Math.floor(Math.random() * 1 + 1));
+  let increment = 0
+  let min = 1
+  let max = 10
+  var correctNumber = numberCreator()
 
+  function numberCreator() {
+    return ((Math.floor(Math.random() * 1 + 1)) + increment);
+  }
 
-  var checkGuess = function() {
-   var user_guess = parseInt(document.getElementById('guess-input').value, 10);
-   if (user_guess === correctNumber) {
-    document.getElementById('last-guess').innerHTML = (`${user_guess}`);
+  function guessValidator(userGuess) {
+    if (userGuess > max || userGuess < min) {
+    document.getElementById('feedback').innerHTML = (`out of range`);
+    return true;
+  } else if (Number.isNaN(userGuess)){
+    document.getElementById('feedback').innerHTML = (`Not a number`);
+    return true;
+  } else
+    return false;
+  }
+
+  function checkGuess(userGuess) {
+   if (userGuess === correctNumber) {
+    document.getElementById('last-guess').innerHTML = (`${userGuess}`);
     document.getElementById('feedback').innerHTML = (`BOOM! That Is Correct!`);
-  } else if (user_guess > correctNumber) {
-    document.getElementById('last-guess').innerHTML = (`${user_guess}`);
+  } else if (userGuess > correctNumber) {
+    document.getElementById('last-guess').innerHTML = (`${userGuess}`);
     document.getElementById('feedback').innerHTML = (`That is too high!`);
-  } else if (user_guess < correctNumber) {
-    document.getElementById('last-guess').innerHTML = (`${user_guess}`);
+  } else if (userGuess < correctNumber) {
+    document.getElementById('last-guess').innerHTML = (`${userGuess}`);
     document.getElementById('feedback').innerHTML = (`That is too low!`);
   } else
     document.getElementById('feedback').innerHTML = (`please enter a valid value`);
   }
 
+
   //listens for a click on the guess button and checks the guess
-  document.getElementById("guess").addEventListener("click", checkGuess)
+  document.getElementById("guess").addEventListener("click", () => {
+    let userGuess = parseInt(document.getElementById('guess-input').value, 10);
+     if (guessValidator(userGuess)) { return true; }
+     checkGuess(userGuess);
+  });
